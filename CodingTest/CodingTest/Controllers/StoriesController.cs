fb3d2fd199daa9672807961a6041317475fe07f0
pratz272 <90966792+pratz272@users.noreply.github.com>
2023-08-07 14:48:31 +0000

    Add files via upload

---
+++ CodingTest/CodingTest/Controllers/StoriesController.cs
@@ -0,0 +1,52 @@
+﻿using CodingTest;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingTest
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StoriesController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public StoriesController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBestStories(int n)
+        {
+            if (n <= 0)
+            {
+                return BadRequest("Invalid value for 'n'");
+            }
+
+            var client = _httpClientFactory.CreateClient("CodingTest");
+            var bestStoryIds = await client.GetFromJsonAsync<int[]>("beststories.json");
+
+            var stories = new List<StoryDisplay>();
+
+            foreach (var storyId in bestStoryIds.Take(n))
+            {
+                var story = await client.GetFromJsonAsync<Story>($"item/{storyId}.json");
+                if (story != null)
+                {
+                    StoryDisplay strDisp = new StoryDisplay();
+                    strDisp.Title = story.Title;
+                    strDisp.Uri = story.Uri;
+                    strDisp.PostedBy = story.PostedBy;
+                    strDisp.Time = DateTimeOffset.FromUnixTimeSeconds(story.Time).UtcDateTime;
+                    strDisp.CommentCount = story.CommentCount;
+                    strDisp.Score = story.Score;
+                    stories.Add(strDisp);
+                }
+            }
+
+            stories.Sort((a, b) => b.Score.CompareTo(a.Score));
+
+            return Ok(stories);
+        }
+    }
+
+}
---
+++ CodingTest/CodingTest/Startup.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using AspNetCoreRateLimit;
+
+namespace CodingTest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            services.AddMemoryCache();
+            services.Configure<IpRateLimitOptions>(Configuration.GetSection("IpRateLimiting"));
+            services.Configure<IpRateLimitPolicies>(Configuration.GetSection("IpRateLimitPolicies"));
+            services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+            services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+            services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+            services.AddSingleton<IProcessingStrategy, AsyncKeyLockProcessingStrategy>();
+            services.AddInMemoryRateLimiting();
+            services.AddHttpClient("CodingTest", client =>
+            {
+                client.BaseAddress = new Uri("https://hacker-news.firebaseio.com/v0/");
+            });
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseIpRateLimiting();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });            
+        }
+    }
+}
